1d1578450e1574ab0d565976f06437b5ce56be46
ethanwack <57767635+ethanwack@users.noreply.github.com>
2025-06-21 15:20:32 -0400

    Initial Commit

---
+++ ChessGame/ChessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessGameApp.swift
+//  ChessGame
+//
+//  Created by Ethan Wacker on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ChessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessGameTests/ChessGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChessGameTests.swift
+//  ChessGameTests
+//
+//  Created by Ethan Wacker on 6/21/25.
+//
+
+import Testing
+@testable import ChessGame
+
+struct ChessGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
